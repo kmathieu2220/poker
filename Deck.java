f4beebcaf7431ec1ee5188bb324330091f2c1cc7
Kyle Mathieu <kmathieu2220@gmail.com>
2012-02-17 10:14:03 -0500

    Changed name to K.

--- Deck.java
+++ Deck.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 /**
  * Write a description of class Deck here.
  * 
- * @author (your name) 
+ * @author (K) 
  * @version (a version number or a date)
  */
 public class Deck
